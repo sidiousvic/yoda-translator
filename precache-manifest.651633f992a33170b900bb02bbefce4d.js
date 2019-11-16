694892e93b263d45e9b728520cca8f96b26ad0c2
sidiousvic <victor6rene@gmail.com>
2019-11-16 17:03:51 +0900

    Updates

---
+++ precache-manifest.651633f992a33170b900bb02bbefce4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c822174682372e7966a0447f4cd93a4d",
+    "url": "/yoda-translator/index.html"
+  },
+  {
+    "revision": "76b575692ddc9853e823",
+    "url": "/yoda-translator/static/css/main.03458536.chunk.css"
+  },
+  {
+    "revision": "05d69e13ca907bb946be",
+    "url": "/yoda-translator/static/js/2.d14e429e.chunk.js"
+  },
+  {
+    "revision": "76b575692ddc9853e823",
+    "url": "/yoda-translator/static/js/main.a710d02e.chunk.js"
+  },
+  {
+    "revision": "976cc092f1bca6b8a223",
+    "url": "/yoda-translator/static/js/runtime-main.565aec6c.js"
+  },
+  {
+    "revision": "3682a5f1f1c985d4ce48eed68f0ae479",
+    "url": "/yoda-translator/static/media/yoda-background.3682a5f1.jpg"
+  },
+  {
+    "revision": "09ea0a497b07d2e78d3f4542f2ea37c2",
+    "url": "/yoda-translator/static/media/yoda-head.09ea0a49.png"
+  }
+]);
